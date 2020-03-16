0ff64f178af205bfbef8b4358dece4c2a10c5f46
rowow <zwh829@qq.com>
2020-03-16 17:49:25 +0800

    用户管理页面的实现和table和form组件封装

---
+++ src/components/CommonTable.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="common-table">
+    <el-table :data="tableData" stripe height="90%" v-loading="config.loading">
+      <el-table-column label="序号" width="85">
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{
+            (config.page - 1) * 20 + scope.$index + 1
+          }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+        v-for="item in tableLabel"
+        :key="item.prop"
+        :label="item.label"
+        :width="item.width ? item.width : 125"
+        show-overflow-tooltip
+      >
+        <template slot-scope="scope">
+          <span style="margin-left: 10px">{{ scope.row[item.prop] }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="handleEdit(scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="handleDelete(scope.row)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      class="page"
+      layout="prev, pager, next"
+      :total="config.total"
+      :current-page.sync="config.page"
+      @current-change="changePage"
+    >
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tableData: Array,
+    tableLabel: Array,
+    config: Object
+  },
+  methods: {
+    handleEdit(row) {
+      this.$emit("edit", row);
+    },
+    handleDelete(row) {
+      this.$emit("del", row);
+    },
+    changePage(page) {
+      this.$emit("changePage", page);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.common-table {
+  height: calc(100% - 62px);
+  background-color: #fff;
+  position: relative;
+  .page {
+    position: absolute;
+    bottom: 0;
+    right: 20px;
+  }
+}
+</style>
--- src/views/UserPage/UserPage.vue
+++ src/views/UserPage/UserPage.vue
@@ -1,9 +1,216 @@
 <template>
-  <div>UserPage</div>
+  <div class="manage">
+    <el-dialog
+      :title="operateType === 'add' ? '新增用户' : '更新用户'"
+      :visible.sync="isShow"
+    >
+      <common-form
+        :form="operateForm"
+        :formLabel="operateFormLabel"
+      ></common-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="isShow = false">取 消</el-button>
+        <el-button type="primary" @click="comfirm">
+          确 定
+        </el-button>
+      </span>
+    </el-dialog>
+    <div class="manage-header">
+      <el-button @click="addUser" type="primary"> + 新增</el-button>
+      <common-form inline :formLabel="formLabel" :form="searchForm">
+        <el-button @click="getList(searchForm.keyword)" type="primary">
+          搜索
+        </el-button>
+      </common-form>
+    </div>
+    <common-table
+      :tableData="tableData"
+      :tableLabel="tableLabel"
+      :config="config"
+      @changePage="getList"
+      @edit="editUserInfo"
+      @del="delUserInfo"
+    ></common-table>
+  </div>
 </template>
 
 <script>
-export default {};
+import CommonForm from "@/components/CommonForm";
+import CommonTable from "@/components/CommonTable";
+export default {
+  data() {
+    return {
+      isShow: false,
+      operateType: "add",
+      // 需要更新的字段名字
+      operateForm: {
+        name: "",
+        age: "",
+        sex: "",
+        birth: "",
+        addr: ""
+      },
+      operateFormLabel: [
+        // 绑定operateForm的字段
+        {
+          model: "name",
+          label: "姓名"
+        },
+        {
+          model: "age",
+          label: "年龄"
+        },
+        {
+          model: "sex",
+          label: "性别",
+          type: "select",
+          opts: [
+            {
+              label: "女",
+              value: 0
+            },
+            {
+              label: "男",
+              value: 1
+            }
+          ]
+        },
+        {
+          model: "birth",
+          label: "出生日期",
+          type: "date"
+        },
+        {
+          model: "addr",
+          label: "地址"
+        }
+      ],
+      searchForm: {
+        keyword: ""
+      },
+      formLabel: [
+        {
+          // 通过model找到searchForm的keyword并绑定
+          model: "keyword",
+          label: ""
+        }
+      ],
+      tableData: [],
+      tableLabel: [
+        {
+          prop: "name",
+          label: "姓名"
+        },
+        {
+          prop: "age",
+          label: "年龄"
+        },
+        {
+          prop: "sexLabel",
+          label: "性别"
+        },
+        {
+          prop: "birth",
+          label: "出生日期",
+          width: 200
+        },
+        {
+          prop: "addr",
+          label: "地址",
+          width: 320
+        }
+      ],
+      config: {
+        page: 1,
+        total: 30,
+        loading: false
+      }
+    };
+  },
+  methods: {
+    getList(name = "") {
+      this.config.loading = true;
+      this.$http
+        .get("/api/user/getUser", {
+          params: {
+            page: this.config.page,
+            name
+          }
+        })
+        .then(res => {
+          this.tableData = res.data.list.map(item => {
+            item.sexLabel = item.sex === 0 ? "女" : "男";
+            return item;
+          });
+          this.config.total = res.data.count;
+          this.config.loading = false;
+        });
+    },
+    addUser() {
+      this.operateForm = {};
+      this.operateType = "add";
+      this.isShow = true;
+    },
+    editUserInfo(item) {
+      this.isShow = true;
+      this.operateType = "edit";
+      this.operateForm = item;
+    },
+    delUserInfo(item) {
+      this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let id = item.id;
+          this.$http
+            .get("/api/user/del", {
+              params: { id }
+            })
+            .then(res => {
+              console.log(res.data);
+              this.$message({
+                type: "success",
+                message: "删除成功!"
+              });
+              this.getList();
+            });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    // 确认更新
+    comfirm() {
+      if (this.operateType === "edit") {
+        this.$http.post("/api/user/edit", this.operateForm).then(res => {
+          this.isShow = false;
+          this.getList();
+          console.log(res.data);
+        });
+      } else if (this.operateType === "add") {
+        this.$http.post("/api/user/add", this.operateForm).then(res => {
+          this.isShow = false;
+          this.getList();
+          console.log(res.data);
+        });
+      }
+    }
+  },
+  created() {
+    this.getList();
+  },
+  components: {
+    CommonForm,
+    CommonTable
+  }
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import "~@/assets/scss/common.scss";
+</style>
